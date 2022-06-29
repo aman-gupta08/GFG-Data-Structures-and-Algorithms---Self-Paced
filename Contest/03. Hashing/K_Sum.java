c4584fd3aa405b3439d793c880da4eb8b34b9bc1
Aman Gupta <95244527+aman-gupta08@users.noreply.github.com>
2022-06-29 14:42:17 +0530

    Update K_Sum.java

--- Contest/03. Hashing/K_Sum.java
+++ Contest/03. Hashing/K_Sum.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 class K_Sum {
   public static void main(String[] args) {
-        Scanner sc = new Scanner();
+        Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while (t-- > 0) {
             int n = sc.nextInt();
